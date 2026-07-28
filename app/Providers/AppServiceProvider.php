9a3187893221babef8bffa3e1fcd73dd3ba212d2
ILHAM MUHTAR <ilhammuhtar607011@gmail.com>
2026-07-28 20:15:16 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,10 +9,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    public function boot(): void
+{
+    if ($this->app->environment('production')) {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
+}
 
     /**
      * Bootstrap any application services.
